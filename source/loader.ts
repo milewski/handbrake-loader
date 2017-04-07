c1451df102abf9bc9fba970b41bfa2ca68fc0518
Rafael <milewski@Rafaels-Mac-mini.local>
2017-04-07 18:30:21 +0800

    Finished Up

--- source/loader.ts
+++ source/loader.ts
@@ -1,45 +1,39 @@
 import { getOptions } from "loader-utils";
-let HandbrakeCLIPath = require('handbrake-node')
-import * as execBuffer from 'exec-buffer';
-import * as path from 'path';
+import { HandbrakeCLIPath } from "handbrake-bin";
+import * as execBuffer from "exec-buffer";
 import * as toSpawnArgs from "object-to-spawn-args";
 import { toDashed } from "./helpers";
 import { OptionsInterface } from "./interfaces/OptionsInterface";
 
 export const raw = true;
-export default function(content) {
+export default function (content) {
 
-    let tempOptions = getOptions(this) || {};
-    let options: OptionsInterface = { enabled: true }
+    let tempOptions = getOptions(this) || {},
+        options: OptionsInterface = { enabled: true }
 
     for (let key in tempOptions) {
-        options[ toDashed(key) ] = tempOptions[ key ]
+        options[toDashed(key)] = tempOptions[key]
     }
+
+    if (options.enabled === false) return content
 
     /**
-     * If there is no options, then there is nothing to do here
+     * Remove enabled from the options object as it is an invalid option for HandBrake
      */
-    if (options.enabled === false) {
-        return content
-    }
+    delete options.enabled;
 
+    /**
+     * Im not sure if this will bring any side effect.. but it seems to work
+     */
+    this.resource = this.resource.replace(/\.[^.]+$/, '.' + options.format)
+
+    const args = ['--input', execBuffer.input, '--output', execBuffer.output]
     const callback = this.async();
-
-    console.log(HandbrakeCLIPath)
-
-    return;
-
-    this.resource = this.resource.replace('.mkv', `.${options.format}`)
 
     execBuffer({
         input: content,
-        bin: handBrake.HandbrakeCLIPath,
-        args: [ '--input', execBuffer.input, '--output', execBuffer.output ].concat(toSpawnArgs(options), { quote: true })
-    }).then(data => {
-        callback(null, data)
-    }).catch(err => {
-        callback(err)
-    });
-
+        bin: HandbrakeCLIPath,
+        args: args.concat(toSpawnArgs(options), { quote: true })
+    }).then(data => callback(null, data)).catch(error => callback(error));
 
 }
--- test/test.ts
+++ test/test.ts
@@ -1,25 +1,41 @@
 import { default as loader } from "../source/loader";
-import * as expect from "expect.js";
-import * as fs from "fs";
+import * as fs from "fs-extra";
 import * as path from "path";
+import * as expect from "expect.js";
+import * as fileType from "file-type";
+import * as webpack from "webpack";
 
-const sampleMKV = fs.readFileSync(path.resolve(__dirname, 'sample.mkv'))
+const resource = path.resolve(__dirname, 'sample-files/sample.mkv');
+const sampleMKV = fs.readFileSync(resource)
+
+function cleanUp() {
+    try {
+        fs.removeSync(path.resolve(__dirname, 'artifacts'))
+    } catch (e) {
+        // do nothing
+    }
+}
 
 describe('Loader', () => {
 
-    it('should not proceed if it has been disabled', done => {
+    before(() => cleanUp())
+    after(() => cleanUp())
+
+    it('should works', done => {
 
         let context = {
-            loader,
+            loader, resource,
             query: {
-                nothing: false
+                format: 'mp4',
             },
             async(){
                 return (error, buffer) => {
 
                     if (error) return done(error);
 
-                    console.log(buffer)
+                    expect(sampleMKV).to.not.eql(buffer)
+                    expect(fileType(buffer)).to.eql({ ext: 'mp4', mime: 'video/mp4' })
+
                     done()
 
                 }
@@ -27,6 +43,55 @@ describe('Loader', () => {
         }
 
         context.loader(sampleMKV)
+
+    });
+
+    it('should throw no error when used with webpack', done => {
+
+        const compiler = webpack({
+            context: __dirname,
+            entry: { app: './app.js' },
+            output: {
+                path: __dirname + '/artifacts',
+                filename: '[name].js'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.mkv$/,
+                        use: [
+                            { loader: 'file-loader', options: { name: '[name].[ext]' } },
+                            {
+                                loader: require.resolve('../source/loader'),
+                                options: {
+                                    format: 'mp4',
+                                    srtFile: path.resolve(__dirname, 'sample-files/subtitle.srt'),
+                                    srtLang: 'eng',
+                                    srtBurn: 1,
+                                    preset: 'Very Fast 1080p30',
+                                    optimize: true
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        });
+
+        compiler.run((error, stats) => {
+
+            if (error) done(error)
+
+            expect(stats.hasErrors()).to.be(false)
+            expect(fs.existsSync(path.join(__dirname, 'artifacts/sample.mp4'))).to.be(true);
+
+            let jsFile = fs.readFileSync(path.join(__dirname, 'artifacts/app.js'), 'utf8');
+
+            expect(/sample\.mp4/.test(jsFile)).to.be(true)
+
+            done()
+
+        });
 
     });
 
